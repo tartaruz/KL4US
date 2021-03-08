b5e7cbe453f2db349f2a31aac73ce62ba99b4e87
Thomas 'Tartarux' Ramirez <thomasrf96@gmail.com>
2021-03-08 18:13:24 +0100

    Try to fix none

--- CBF/NLP.py
+++ CBF/NLP.py
@@ -2,23 +2,53 @@ import pandas as pd
 import pickle
 import spacy
 from spacy.lang.nb import Norwegian
-from spacy.tokenizer import Tokenizer
+# from spacy.tokenizer import Tokenizer
 import string
 import nltk
 from nltk.corpus import stopwords
-nltk.download('stopwords')
+from nltk.stem.snowball import NorwegianStemmer 
+  
 
+
+stemmer = NorwegianStemmer() 
+
+nltk.download('stopwords')
+# print([f+"\n" for f in dir(Norwegian())])
 
 class NLP():
     def __init__(self, data):
         self.data = data
-        self.nlp = Norwegian()
+        self.nlp = spacy.load('nb_core_news_sm')
 
 
+    def fix_none_titles(self):
+        doc_id2title = {}
+        cols = ["documentId", "title"]
+        titles = []
+        nr = 0
+        for index, row in self.data[cols].iterrows():
+            if row["title"] == None:
+                nr += 1
+            key = row[cols[0]]
+            if key not in doc_id2title:
+                doc_id2title[key] = row[cols[1]]
+        print(nr)
+        
+        for index, row in self.data[cols].iterrows():
+            if row["title"] == None:
+                nr-=1
+                titles.append(doc_id2title[row["documentId"]])
+            else:
+                titles.append(row["title"])
+        
+        print(nr)
+        self.data["title"] = titles
+
+    
     def get_titles(self):
         titles = self.data["title"]
         titles = [title for title in titles]
-        return titles[:10]
+        return titles
 
     def remove_noice(self, titles):
         PUNCT = string.punctuation+"«»"
@@ -30,8 +60,13 @@ class NLP():
         return titles
 
     def normalize(self, titles):
-        #lemmatazion or port stemmer?
-        
+        #lemmatazion or portstemmer?
+        # lemmatizer = self.nlp.lemmatizer()
+
+        for index, title in enumerate(titles):
+            # doc = self.nlp(" ".join(title))
+            # titles[index] = doc[0].token
+            titles[index] = [stemmer.stem(word) for word in title]
         
         return titles
 
@@ -47,6 +82,7 @@ class NLP():
         return titles
 
     def do_magic(self):
+        self.fix_none_titles()
         titles = self.get_titles()
 
         titles = self.remove_noice(titles)
@@ -55,14 +91,15 @@ class NLP():
         
         titles = self.remove_stopwords(titles)
         
-        print(titles)
-        # title = self.normalize(titles)
+        title = self.normalize(titles)
+        self.data["title"] = titles
 
-    def compare(self):
+    def TFIDF(self):
         pass
+    
 
 
-
+    
 
 path = "CBF/data/df_pickled"
 df = pickle.load(open(path, "rb"))
@@ -70,8 +107,8 @@ print([col for col in df])
 
 nlp = NLP(df)
 
+nlp.fix_none_titles()
 nlp.do_magic()
-
 
 
 
