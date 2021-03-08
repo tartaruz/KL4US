5caa2e4dcd3e8fcaa9d98bb12709f31856aa1611
Thomas 'Tartarux' Ramirez <thomasrf96@gmail.com>
2021-03-08 15:24:12 +0100

    CBF

---
+++ CBF/NLP.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import pickle
+
+class NLP():
+    def __init__(self, data):
+        self.data = data
+
+
+
+
+
+
+
+path = "data/df_pickled"
+
+df = pickle.load(open(path, "rb"))
+
+
+
+
+
+
+
