57edfd8cf8576a64c458d7639accddaa3afb00d8
Thomas 'Tartarux' Ramirez <thomasrf96@gmail.com>
2021-03-09 14:12:14 +0100

    NLP implemented, missing how the data should be returned in the compare fucntion

--- CBF/NLP.py
+++ CBF/NLP.py
@@ -7,7 +7,9 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.snowball import NorwegianStemmer 
-  
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 
 stemmer = NorwegianStemmer() 
@@ -19,70 +21,51 @@ class NLP():
     def __init__(self, data):
         self.data = data
         self.nlp = spacy.load('nb_core_news_sm')
+        self.vectors = None
 
 
-    def fix_none_titles(self):
-        doc_id2title = {}
-        cols = ["documentId", "title"]
-        titles = []
-        nr = 0
-        for index, row in self.data[cols].iterrows():
-            if row["title"] == None:
-                nr += 1
-            key = row[cols[0]]
-            if key not in doc_id2title:
-                doc_id2title[key] = row[cols[1]]
-        print(nr)
-        
-        for index, row in self.data[cols].iterrows():
-            if row["title"] == None:
-                nr-=1
-                titles.append(doc_id2title[row["documentId"]])
-            else:
-                titles.append(row["title"])
-        
-        print(nr)
-        self.data["title"] = titles
+    def preprosses_df(self):
+        self.info("Removing None and duplicates in data")
+        self.data = self.data.drop_duplicates(["title"])
+        self.data = self.data.dropna(subset=('title',))
 
-    
+  
     def get_titles(self):
+        self.info("Getting the titles")
         titles = self.data["title"]
         titles = [title for title in titles]
         return titles
 
     def remove_noice(self, titles):
+        self.info("Removing the noice in the titles")
         PUNCT = string.punctuation+"«»"
-
         for index, title in enumerate(titles):
-            #Remove punctutations
             titles[index] = title.translate(str.maketrans('', '', PUNCT))
-
         return titles
 
     def normalize(self, titles):
-        #lemmatazion or portstemmer?
-        # lemmatizer = self.nlp.lemmatizer()
-
+        self.info("Stemming the words")
         for index, title in enumerate(titles):
-            # doc = self.nlp(" ".join(title))
-            # titles[index] = doc[0].token
             titles[index] = [stemmer.stem(word) for word in title]
-        
         return titles
 
     def remove_stopwords(self, titles):
+        self.info("Removing the stopwords in the titles")
+
         for index, title in enumerate(titles):
             titles[index] = [word for word in title if not word in stopwords.words("norwegian")]
         return titles
 
     
     def tokenize(self, titles):
+        self.info("Tokenizing and lowering the word in titles")
         for index in range(len(titles)):
             titles[index] =  nltk.word_tokenize(titles[index].lower())
         return titles
 
     def do_magic(self):
-        self.fix_none_titles()
+        self.info("Init prosses")
+        self.preprosses_df()
         titles = self.get_titles()
 
         titles = self.remove_noice(titles)
@@ -91,25 +74,53 @@ class NLP():
         
         titles = self.remove_stopwords(titles)
         
-        title = self.normalize(titles)
-        self.data["title"] = titles
+        titles = self.normalize(titles)
+
+        self.data["new_title"] = titles
+        self.TFIDF()
 
     def TFIDF(self):
-        pass
-    
+        self.info("Vectorizing the corpus with TFIDF")
+        vectorizer = TfidfVectorizer()
+        corpus = [" ".join(sentenc) for sentenc in self.data["new_title"].tolist()]
+        self.vectors = vectorizer.fit_transform(corpus)
+        # denselist = vectors.todense().tolist()
+
+    def compare(self, title, k=10):
+        self.info("Comparing title with all other titles")
+        q_index = self.data[self.data["title"]==title].index
+        
+        if len(q_index)<=0:
+            return None
+        
+        similarity = cosine_similarity(self.vectors)[q_index].tolist()[0]
+        similarities = [(i,v) for i,v in enumerate(similarity)]
+        similarities.sort(key=lambda x:x[1], reverse=True)
+        
+        #Remove itself since it compares to all enetries in df
+        similarities.pop(0)
+        results = [self.data.iloc[res[0]] for res in similarities]
+        return similarities[:k+1], results
+
+    def info(self, text):
+        line = "="*(75-len(text))
+        print(f"{line}=> {text}")
+
 
 
     
 
 path = "CBF/data/df_pickled"
 df = pickle.load(open(path, "rb"))
-print([col for col in df])
-
+title = df["title"][69]
 nlp = NLP(df)
 
-nlp.fix_none_titles()
 nlp.do_magic()
 
+res, res_array = nlp.compare(title)
+print(res_array)
+
+# print(nlp.data["title"])
 
 
 
